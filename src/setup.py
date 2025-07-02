7d92f933271489ed66fe710db5b8705767cad6b3
Rongjiang Tang <88586717+Rongjiang007@users.noreply.github.com>
2025-07-02 17:10:59 +0800

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,1 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
 
-ext = Extension(
-    name="ang_dis_strain",
-    sources=["ang_dis_strain.pyx", "ang_dis_strain.cpp"],
-    language="c++",
-    extra_compile_args=["-std=c++11"]
-)
-
-setup(
-    ext_modules=cythonize(ext),
-)
