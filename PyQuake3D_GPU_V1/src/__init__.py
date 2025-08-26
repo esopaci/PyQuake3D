538b42acd1b31b69980e051d44311c45d298d93b
Rongjiang Tang <88586717+Rongjiang007@users.noreply.github.com>
2025-08-26 16:53:52 +0800

    Delete PyQuake3D_GPU_V1 directory

--- PyQuake3D_GPU_V1/src/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/__init__.py
-from . import readmsh
-from . import QDsim
-from . import SH_greenfunction
-from . import DH_greenfunction
-
-__all__ = ["readmsh","QDsim","SH_greenfunction","DH_greenfunction"]
